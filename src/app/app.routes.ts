8c0ca41fdc63b0f84b0c5eac3772445666859b47
Nifraz Navahz <nifraz@live.com>
2024-06-21 22:32:35 +0530

    add not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentsComponent } from './students/students.component';
 import { StudentDetailsComponent } from './students/student-details/student-details.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -23,5 +24,12 @@ export const routes: Routes = [
         path: 'students/:id',
         title: 'Student Details | SmartSchool',
         component: StudentDetailsComponent,
+    },
+
+    //Wild Card Route for 404 request 
+    {
+        path: '**', pathMatch: 'full',  
+        title: '404 | SmartSchool',
+        component: NotFoundComponent 
     },
 ];
--- src/app/students/student-details/student-details.component.ts
+++ src/app/students/student-details/student-details.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { StudentsService, Student } from '../students.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from "../../shared/not-found/not-found.component";
 
 @Component({
-  selector: 'app-student-details',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './student-details.component.html',
-  styleUrl: './student-details.component.scss'
+    selector: 'app-student-details',
+    standalone: true,
+    templateUrl: './student-details.component.html',
+    styleUrl: './student-details.component.scss',
+    imports: [
+        CommonModule,
+        NotFoundComponent
+    ]
 })
 export class StudentDetailsComponent implements OnInit {
 
