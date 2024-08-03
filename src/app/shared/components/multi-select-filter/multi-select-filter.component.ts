79c87aea941246474aecfcf9587f22a2bd6e5232
Nifraz Navahz <nifraz@live.com>
2024-08-03 22:19:05 +0530

    use checkboxes for multi-select filter

--- src/app/shared/components/multi-select-filter/multi-select-filter.component.ts
+++ src/app/shared/components/multi-select-filter/multi-select-filter.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IFilterAngularComp } from 'ag-grid-angular';
 import { IFilterParams, IDoesFilterPassParams } from 'ag-grid-community';
-import { MatFormField } from '@angular/material/form-field';
-import { MatOption, MatSelect } from '@angular/material/select';
+import { MatCheckbox } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-multi-select-filter',
   standalone: true,
   imports: [
-    MatFormField,
-    MatSelect,
-    MatOption,
+    MatCheckbox,
     CommonModule,
     ReactiveFormsModule,
   ],
@@ -22,42 +19,53 @@ import { CommonModule } from '@angular/common';
 export class MultiSelectFilterComponent implements IFilterAngularComp, OnInit {
   params!: CustomFilterParams;
   filterOptions: string[] = [];
-  filterControl = new FormControl<any>([]);
+  formGroup: FormGroup = this.fb.group({});
+
+  constructor(private fb: FormBuilder) {}
 
   agInit(params: CustomFilterParams): void {
     this.params = params;
     if (params.values) {
       this.filterOptions = params.values;
+      this.filterOptions.forEach(option => {
+        this.formGroup.addControl(option, this.fb.control(false));
+      });
     }
 
-    this.filterControl.valueChanges.subscribe(() => {
+    this.formGroup.valueChanges.subscribe(() => {
       this.params.filterChangedCallback();
     });
   }
 
   doesFilterPass(params: IDoesFilterPassParams): boolean {
-    const values = this.filterControl.value;
-    return values?.length === 0 || values?.includes(params.data[this.params.colDef.field!]);
+    const selectedOptions = this.getSelectedOptions();
+    return selectedOptions.length === 0 || selectedOptions.includes(params.data[this.params.colDef.field!]);
   }
 
   isFilterActive(): boolean {
-    return this.filterControl.value.length > 0;
+    return this.getSelectedOptions().length > 0;
   }
 
   getModel(): any {
     if (!this.isFilterActive()) {
       return null;
     }
-    return { value: this.filterControl.value };
+    return { value: this.getSelectedOptions() };
   }
 
   setModel(model: any): void {
-    this.filterControl.setValue(model ? model.value : []);
+    const values = model ? model.value : [];
+    this.filterOptions.forEach(option => {
+      this.formGroup.controls[option].setValue(values.includes(option));
+    });
+  }
+
+  getSelectedOptions(): string[] {
+    return this.filterOptions.filter(option => this.formGroup.controls[option].value);
   }
 
   ngOnInit() {}
 }
-
 
 export interface CustomFilterParams extends IFilterParams {
   values: string[];
